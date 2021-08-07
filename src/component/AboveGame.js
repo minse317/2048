da796560b0c1b439d78248e5b4a90cd98339d2fb
minse17 <s2019w30@e-mirim.hs.kr>
2021-08-07 23:43:55 +0900

    startPage

---
+++ src/component/AboveGame.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function AboveGame() {
+  return (
+    <div className="above-game">
+      <p className="game-intro">
+        Join the numbers and ger to the <stirng>2048 tile!</stirng>
+      </p>
+      <a href="/" calssName="restart-button">
+        New Game
+      </a>
+    </div>
+  );
+}
---
+++ src/component/Hearder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="heading">
+        <h1 className="title">2048</h1>
+        <div className="score-container">
+            <div className="score-container"></div>
+            <div className="best-container"></div>
+        </div>
+    </div>
+  );
+}
